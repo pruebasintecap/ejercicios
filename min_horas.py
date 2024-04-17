53e0c169c117cfd5f52dadbe7017fa5ef9f01b5c
pruebasintecap <167369299+pruebasintecap@users.noreply.github.com>
2024-04-17 16:02:46 -0600

    Add files via upload

---
+++ min_horas.py
@@ -0,0 +1,52 @@
+# hora=int(input("Ingrese la hora: ")) 
+# minutos=int(input("Ingrese el minuto: "))
+# duracion=int(input("Ingrese la duracion del evento: "))
+
+# min_totales=minutos+duracion
+# hora_totales=hora+min_totales//60 
+# min_totales=min_totales%60 
+# hora_totales=hora_totales%24
+
+# print("la hora de finalizar sera: ",hora_totales,":", min_totales)
+
+
+# bisiesto=int(input("Ingrese un año:"))
+# if (bisiesto % 4 == 0) and (bisiesto % 100 != 0) or (bisiesto % 400 == 0):
+#     print("es bisiesto")
+# else:
+#     print("No es bisiesto")
+
+
+
+
+
+
+
+    
+# if (bisiesto%100 == 0)and(bisiesto%400==0):
+#     if (bisiesto%4==0) and (bisiesto%100!=0):
+#         print("el año:", bisiesto, "es un año bisiesto") 
+# else:
+#     print("No es bisiesto")
+
+
+# e1=int(input("Ingrese edad 1: "))
+# e2=int(input("Ingrese edad 2: "))
+# e3=int(input("Ingrese edad 3: "))
+# e4=int(input("Ingrese edad 4: "))
+# e5=int(input("Ingrese edad 5: "))
+# e6=int(input("Ingrese edad 6: "))
+# e7=int(input("Ingrese edad 7: "))
+# e8=int(input("Ingrese edad 8: "))
+# pre=0 #contador de edades de 4 años
+# kin=0 #contador de edades de 5 años
+# pri=0 #contador de edades de 6 años
+
+# if (3<e1<=6 ) and (3<e2<=6 ) and (3<e3<=6 ) and (3<e4<=6 ) and (3<e5<=6 ) and (3<e6<=6 ) and (3<e7<=6 ) and (3<e8<=6 ):
+#     if e1==4 or e2==4 or e3==4 or e4==4:
+#         pre=pre+1
+#     print(pre, " Niño de prekinder")
+# else:
+#     print("no valido")
+
+
